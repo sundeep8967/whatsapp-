24ca12bf2aa86142257c02c994c69730ef85d465
manoj-kumar-m <manojkumarm0136@gmail.com>
2023-01-26 16:26:17 +0530

    added bottom navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,22 +1,38 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {View,Text} from 'react-native'
-import ChatScreen from '../Screens/ChatScreen';
-import ChatsScreen from '../Screens/ChatsScreen';
+import ChatScreen from '../screens/ChatScreen';
+import ChatsScreen from '../screens/ChatsScreen';
+// import ChatScreen from "../screens/ChatScreen/ChatScreen";
+import ContactsScreen from "../screens/ContactsScreen";
+import GroupInfoScreen from "../screens/GroupInfoScreen";
+import NewGroupScreen from "../screens/NewGroupScreen";
+import AddContactsToGroupScreen from "../screens/AddContactsToGroupScreen";
+import MainTabNavigator from "./MainTabNavigator";
 
 const Stack=createNativeStackNavigator();
 
 const Navigator = () => {
   return (
-   <NavigationContainer >
-    
-    <Stack.Navigator initialRouteName='Chats'>
-
-    <Stack.Screen name="Chats" component={ChatsScreen}/>
-    <Stack.Screen name="Chat" component={ChatScreen}/>
-    
+    <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{ headerStyle: { backgroundColor: "whitesmoke" } }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={MainTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Group Info" component={GroupInfoScreen} />
+      <Stack.Screen name="Contacts" component={ContactsScreen} />
+      <Stack.Screen name="New Group" component={NewGroupScreen} />
+      <Stack.Screen
+        name="Add Contacts"
+        component={AddContactsToGroupScreen}
+      />
     </Stack.Navigator>
-   </NavigationContainer>
+  </NavigationContainer>
   )
 }
 
